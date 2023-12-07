245ea2c411d4d7ae09aea0785c88cbe7915270c0
StasLambant <39237381+StasLambant@users.noreply.github.com>
2023-12-07 23:29:26 +0000

    API return

--- app.py
+++ app.py
@@ -8,16 +8,24 @@ app = Flask(__name__)
 # Set the OpenAI API key
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
+# Global variable to store the thread ID
+global_thread_id = None
+
 def create_thread(ass_id, prompt):
-    # Create a thread
-    thread = openai.beta.threads.create()
-    my_thread_id = thread.id
+    global global_thread_id
+
+    # Check if a thread already exists, create one if not
+    if not global_thread_id:
+        thread = openai.beta.threads.create()
+        global_thread_id = thread.id
+
+    my_thread_id = global_thread_id
 
     # Create a message
     message = openai.beta.threads.messages.create(
         thread_id=my_thread_id,
         role="user",
-        content=prompt
+        content=prompt  
     )
 
     # Run
@@ -26,8 +34,9 @@ def create_thread(ass_id, prompt):
         assistant_id=ass_id,
     ) 
 
-    return run.id, thread.id
+    return run.id, my_thread_id
 
+#check run status
 def check_status(run_id, thread_id):
     run = openai.beta.threads.runs.retrieve(
         thread_id=thread_id,
@@ -52,16 +61,19 @@ def chat():
 
         while status != "completed":
             status = check_status(my_run_id, my_thread_id)
-            time.sleep(2)
+            time.sleep(1)
 
         response = openai.beta.threads.messages.list(
             thread_id=my_thread_id
         )
-
+        print("Sending response with Thread ID:", response)  # Logs the response being sent back
+        return jsonify({'response': f"{response}\n\nThread ID: {my_thread_id}"})
+        '''
         if response.data:
-            chat_response = response.data[0].content[0].text.value
-            print("Sending response:", chat_response)  # Logs the response being sent back
-            return jsonify({'response': chat_response})
+            chat_response = response.data[-1].content[0].text.value
+            print("Sending response with Thread ID:", chat_response)  # Logs the response being sent back
+            return jsonify({'response': f"{chat_response}\n\nThread ID: {my_thread_id}"})
+        '''
     except Exception as e:
         print("Error:", str(e))  # Logs the error if occurred
         return jsonify({'error': str(e)})
